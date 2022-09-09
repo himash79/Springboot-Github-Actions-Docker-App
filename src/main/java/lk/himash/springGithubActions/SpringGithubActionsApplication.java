5e0e9c67af681b02829a53abfd0dbcbfd1cf882a
himash79 <himashbandara79@gmail.com>
2022-09-09 14:23:34 +0530

    add logs

--- src/main/java/lk/himash/springGithubActions/SpringGithubActionsApplication.java
+++ src/main/java/lk/himash/springGithubActions/SpringGithubActionsApplication.java
@@ -1,5 +1,7 @@
 package lk.himash.springGithubActions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class SpringGithubActionsApplication {
+	
+	private final static Logger log = LoggerFactory.getLogger(SpringGithubActionsApplication.class);
 
 	@GetMapping("/getMessage")
 	public String getMessage() {
+		log.info("Application start successfully...!");
 		return "Message Read Successsfully...!";
 	}
 	
